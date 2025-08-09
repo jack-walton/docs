3b95ee7003b525dbf46d4bdb711bf8cbd2d5ef86
Jack Spencer Walton <132408856+jack-walton@users.noreply.github.com>
2025-08-09 18:22:57 -0500

    Reorganize page structure
    I'm having issues with the _meta.js files and the page structure and navigation doesn't work as expected. I  think this should fix most issues.

--- content/How to Use the AXS App/_meta.js
+++
@@ -1,7 +0,0 @@
-export default {
-  "overview": "Overview",
-  "sign-in": "Sign In",
-  "navigate-the-app": "Navigate the App",
-  "search-for-events": "Search for Events",
-  "buy-tickets": "Buy Tickets",
-}
--- content/_meta.js
+++ content/_meta.js
@@ -1,5 +1,42 @@
 export default {
-  "index": "Introduction",
-  "How to Use the AXS App": "How to Use the AXS App",
-
+  "about": { 
+    "title": "About",
+    type: "page",
+  },
+  "index": {
+    "title": "Overview",
+    "theme": {
+      breadcrumb: false,
+    }
+  },
+  "navigate-the-app": {
+  "title": "Navigate the App",
+  "theme": {
+    breadcrumb: false,
+    }
+  },
+  "create-an-account": {
+  "title": "Create an Account",
+  "theme": {
+      breadcrumb: false,
+    },
+  },
+  "sign-in": {
+    "title": "Sign In",
+    "theme": {
+      breadcrumb: false,
+    },
+  },
+  "search-for-events": {
+    "title": "Search for Events",
+    "theme": {
+      breadcrumb: false,
+    }
+  },
+  "buy-tickets": {
+    "title": "Buy Tickets",
+    "theme": {
+      breadcrumb: false,
+    }
+  },
 }
