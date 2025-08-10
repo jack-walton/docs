104dd410de48b801177c11a585191bf887de230b
Jack Spencer Walton <132408856+jack-walton@users.noreply.github.com>
2025-08-10 11:09:59 -0500

    Update GitHub repository

--- app/layout.tsx
+++ app/layout.tsx
@@ -49,7 +49,7 @@ export default async function RootLayout({children}) {
             banner={banner}
             navbar={navbar}
             pageMap={await getPageMap()}
-            docsRepositoryBase="https://github.com/phucbm/nextra-docs-starter/tree/main"
+            docsRepositoryBase="https://github.com/jack-walton/help.jackwalton.xyz/tree/main"
             footer={footer}
             navigation={{
                 prev: true,
