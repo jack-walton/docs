0937d11b553142e75f2c53df29def4b561184417
Jack Spencer Walton <132408856+jack-walton@users.noreply.github.com>
2025-08-30 19:55:33 -0500

    Update 'theme-guide' display title

--- content/chiri/_meta.js
+++ content/chiri/_meta.js
@@ -1,7 +1,7 @@
 const meta = {
   "overview": "Overview of CHIRI",
   "getting-started": "Getting Started",
-   "theme-guide": "Theme Guide",
+   "theme-guide": "Usage Guide",
   "embedded-content": "Embedded Content",
   "KaTeX-example": "KaTeX Example",
  
