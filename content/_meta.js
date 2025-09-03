8b345ea6ef368936b02a41636295833a9fcb8428
Jack Spencer Walton <132408856+jack-walton@users.noreply.github.com>
2025-09-03 10:22:27 -0500

    Remove extra images from landing page

--- content/_meta.js
+++ content/_meta.js
@@ -17,6 +17,27 @@ const meta = {
     },
   },
 
+  "posts" : {
+    "title": "Blog",
+          display: "hidden",
+
+    type: "page",
+    theme: {
+      typesetting: "article",
+      timestamp: false,
+      sidebar: false,
+    },
+  },
+
+  "gallery": {
+    "title": "Gallery",
+    "type": "page",
+    "theme": {
+      "timestamp": false,
+      "sidebar": false,
+    },
+  },
+
   "docs": { 
   "title": "Docs",
     type: "page",
